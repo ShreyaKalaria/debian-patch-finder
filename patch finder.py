790f7c266e27c2c16b60b6b78f9aad75309bfc95
nksegos <nksegos@gmail.com>
2019-03-29 19:54:01 +0200

    Improved error catching. Now entries that are marked as vulnerable by the security tracker, get the links from their notes extracted.

--- patch finder.py
+++ patch finder.py
@@ -17,31 +17,24 @@ year_vln = str(input("Enter the CVE year to query(1999-2019):\n"))
 distribution = str(input("\nEnter the distribution(jessie to sid:\n"))
 query_str = 'CVE-'+year_vln
 print("Searching entries matching pattern: " + query_str)
+
 for line in cve_list:
     if line.startswith(query_str):
         check = ''.join(islice(cve_list, 1))
         if all(x not in check for x in reject_entry):
-            # print(line.split(' ')[0])
             vulns.append(str(line.split(' ')[0]))
-            # print(check)
 
 vuln_codes = list(set(vulns))
-
+fixed_from_source = []
 browser = mechanicalsoup.StatefulBrowser()
 for entry in vuln_codes:
     url = "https://security-tracker.debian.org/tracker/" + entry
-    # print(url)
     browser.open(url)
-    # browser.launch_browser()
-    # print(browser.get_url())
-    # browser.get_current_page()
     try:
         vuln_status = browser.get_current_page().find_all("table")[1]
-    except TypeError:
+    except IndexError:
         print("No info on package vulnerability status")
         continue
-
-    # print(vuln_status)
 
     source = (((vuln_status.select('tr')[1]).select('td')[0]).getText()).replace(" (PTS)", "")
     output = 0
@@ -52,7 +45,19 @@ for entry in vuln_codes:
             parsed_array.append(column.text)
         if len(parsed_array) == 4:
                 if distribution in parsed_array[1]:
-                    print("Source package " + source + " (version " + parsed_array[2] + ")" + " is " + parsed_array[3] + " (" + entry + ")" + " in " + parsed_array[1])
+                    '''print("Source package " + source + " (version " + parsed_array[2] + ")" + " is " + parsed_array[3
+                    ]+ " (" + entry + ")" + " in " + parsed_array[1])
+                    '''
+                    if parsed_array[3] == 'fixed':
+                        fixed_from_source.append(str(source) + ' - ' + str(parsed_array[2]))
+                    else:
+                        try:
+                            vuln_notes = browser.get_current_page().find('pre')
+                            noted_links = vuln_notes.find_all('a')
+                        except (TypeError, AttributeError) as errors:
+                            continue
+                        for link in noted_links:
+                            print(link.get('href') + '\n')
                     output = 1
 
     if output == 0:
